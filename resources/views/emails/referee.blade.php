f4a5ca067e397afc78dfe4e1b1e3a6c4e7f0d499
Marco Dal Monte <marcodm1982@gmail.com>
2022-02-24 20:44:33 +0000

    Changed templates for emails

--- resources/views/emails/referee.blade.php
+++ resources/views/emails/referee.blade.php
@@ -1,5 +1,101 @@
-<div>
-	Caro/a :name :surname
-	<br/>
-	Il tuo codice sconto &eacute; MICROREGALO
-</div>
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Invita un Amico - Microrganismi Effettivi</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700&display=swap" />
+
+        <style type="text/css">
+			.container {
+				margin: 0 auto 44px;
+				width: 90%;
+			}
+
+			.logo-container {
+				height: 76px;
+				padding: 19px 0 12px;
+				text-align: center;
+			}
+
+			.logo-container-light {
+				margin: 0;
+				padding: 0;
+			}
+
+			.logo {
+				height: 40px;
+				width: 259px;
+			}
+
+			.titolo {
+				background-color: #1d1d1d;
+				color: #FFF;	
+				font-family: 'Raleway', sans-serif;
+				margin-bottom: 60px;
+				text-align: center;
+			}
+
+			.titolo h3 {
+				font-size: 24px;
+				padding: 1px;
+			}
+
+			.testo {
+				font-family: 'Raleway', sans-serif;
+				font-size: 22px;
+				text-align: center;
+			}
+
+			.micronative-promo {
+				display: block;
+				height: 681px;
+				margin: 0 auto;
+				width: 650px;
+			}
+			
+			.center {
+				text-align: center;
+			}
+
+			.footer {
+				padding: 56px 0;
+			}
+		</style>
+    </head>
+    <body>
+		<div class="container">
+			<div class="logo-container">
+				<img src="{APP_URL}/public/images/micronative-logo.png" class="logo" alt="MicroNative Logo" title="MicroNative Logo" />
+			</div>
+
+			<p class="testo">Il tuo Amico/a :prospect_name :prospect_surname si é registrato/a sul nostro sito internet.</p>
+			<p class="testo">Per ringraziarti di avercelo presentato, ti offriamo con piacere<br />
+			il Codice Promozionale:</p>
+			<p class="testo">MICROREGALO</p>
+			<p class="testo">Digitalo dove c'è scitto &quot;inserisci codice promozionale&quot; nella sezione carrello,<br />
+			ti darà diritto a ricevere, con il tuo prossimo ordine,</p>
+			<p class="testo">uno sconto immediato del 15% su tutti i prodotti,</p>
+			<p class="testo">un nebulizzatore MicroNative da 500 ml,</p>
+			<p class="testo">e una bottiglia di MicroNative per la pulizia della casa da 250 ml.</p>
+			<p class="testo">Grazie e a presto, MicroNative.</p>
+			<img src="{APP_URL}/public/images/promo-micronative.jpg" class="micronative-promo" alt="Promo MicroNative" title="Promo MicroNative" />
+
+			<div class="footer titolo">
+				<div class="logo-container logo-container-light">
+					<img src="{APP_URL}/public/images/micronative-light.png" class="logo" alt="MicroNative Logo" title="MicroNative Logo" />
+				</div>
+
+				<div class="center">
+					<p>&copy; 2022 Microrganismieffettivi.net - Via Monte Cricco 2B, 37014 Castelnuovo del Garda - Verona</p>
+					<p>Se non vuoi più ricevere comunicazioni, rispondi a questa email scrivendo come oggetto &quot;CANCELLAMI&quot;</p>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
